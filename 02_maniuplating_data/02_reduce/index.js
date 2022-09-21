26bf0e649a618ebb05a7d1b917875c98a23fd884
oreoorbitz <45273923+oreoorbitz@users.noreply.github.com>
2022-09-21 13:10:19 -0500

    Changes

---
+++ 02_maniuplating_data/02_reduce/index.js
@@ -0,0 +1,9 @@
+// store every product card to a variable, using querySelectorAll
+// convert that variable to an array, because querySelectorAll doesn't return an array
+// create a function that takes an array. In the function, map through the array of DOM elements, and extract the price and quantity of each card. multiply the price by the quantity, and return the result
+
+
+// add a click event listener for the get "Update total" button
+// use the function you made above to take all the product cards output the total for each product.
+// use the reduce function to iterate through the array of product totals, and sum all these into a cart total
+// inject the cart total as the innerText of the data-component-total element
