7d84b30a969cbc924e10f73615d91d5d250fc20f
Brianna Foley <brianna.foley87@gmail.com>
2022-09-21 17:27:39 -0500

    exercise 1 - map

--- 02_maniuplating_data/01_map/index.js
+++ 02_maniuplating_data/01_map/index.js
@@ -1,15 +1,27 @@
 // Create a variable containing all the buttons that have the data-component-button attribute
-const buttons = document.querySelectorAll('[data-component-button]')
-buttons.forEach((button) => {
-  button.addEventListener('click', )
-})
-
 // Iterate through all these buttons using a forEach
-
 // Create event listener to check for clicks on the buttons
 
+const buttons = document.querySelectorAll('[data-component-button]')
+buttons.forEach((button) => {
+  button.addEventListener('click', (event) => {
+    const date = new Date()
+    const newEvent = new CustomEvent('clickInfo', {
+      detail: {
+        data: event.target.getAttribute('data-id'),
+        text: event.target.innerText,
+        time: date
+      }
+    })
+    document.dispatchEvent(newEvent)
+  })
+})
+
 /*
-inside of the click event callback dispatch event to the document. When creating this event, create a detail object to pass into the custom event
-Inside of a detail object, add a property with a key of "data", and a value of the data-id dataset attribute, then another property, with a key of "time",
-and value of the current time, using the Date class, and lastly title, with value of the innerText of the button
+inside of the click event callback, dispatch event to the document. When creating
+this event, create a detail object to pass into the custom event
+Inside of a detail object, add a property with a key of "data", and a value of
+the data-id dataset attribute, then another property, with a key of "time",
+and value of the current time, using the Date class, and lastly title, with value
+of the innerText of the button
 */
--- 02_maniuplating_data/01_map/index2.js
+++ 02_maniuplating_data/01_map/index2.js
@@ -1,10 +1,21 @@
 const clicks = []
 
 // create an event listener to listen for the custom "DOM:updated" event
-/* Inside of the callback for the event listener, 
-take the contents of the event.detail object, the text and time of the event, put these two properties into a new object and push to the clicks array
+/* Inside of the callback for the event listener,
+take the contents of the event.detail object, the text and time of the event,
+put these two properties into a new object and push to the clicks array
 */
+const clean = (arr) => arr.map(click => click.data + click.text + click.time).map(string => string.replaceAll(' ', '-'))
 
-// create a new function, call it clean. the function will take a an array, the clicks array. The function will return a new array, with just a single string value, consisting of the id value of the button, the title and the date, every space would be replaced with -
+document.addEventListener('clickInfo', (event) => {
+  clicks.push(new Object(event.detail))
+  const cleanClicks = clean(clicks)
+  console.log(cleanClicks)
+})
+/* create a new function, call it clean. the function will take a an array,
+the clicks array. The function will return a new array, with just a single string value,
+consisting of the id value of the button, the title and the date, every space
+would be replaced with - */
 
-// execute the function with clicks as the argument, and assign to a variable, cleanClicks, console log clean clicks to test
+/* execute the function with clicks as the argument, and assign to a variable,
+cleanClicks, console log clean clicks to test */
