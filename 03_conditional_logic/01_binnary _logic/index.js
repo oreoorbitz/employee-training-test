537f20f0a22031e5d11d3e8506a2aa8415a43e46
oreoorbitz <45273923+oreoorbitz@users.noreply.github.com>
2022-09-24 15:39:57 -0500

    Changes

---
+++ 03_conditional_logic/01_binnary _logic/index.js
@@ -0,0 +1,3 @@
+// use the style attribute of an element to hide or display it by changing the display style from none to initial
+// you'll want to check if the check box is checked or not, there is a built in property for that.
+// there are several things that need to be check if are true or not, try to avoid doing all these checks in a single function and instead put the checks in seperate functions
