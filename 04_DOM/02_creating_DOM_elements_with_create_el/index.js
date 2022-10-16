af752324b21b12ea9112e11ab003e82909eec0a5
oreoorbitz <45273923+oreoorbitz@users.noreply.github.com>
2022-10-16 12:21:50 -0500

    changes

---
+++ 04_DOM/02_creating_DOM_elements_with_create_el/index.js
@@ -0,0 +1,17 @@
+callImaginaryApi = () => [
+  {
+    title: 'product 1',
+    featured_image: 'product-1.jpg',
+    product_id: 15363461
+  },
+  {
+    title: 'product 2',
+    featured_image: 'product-2.jpg',
+    product_id: 15236412
+  },
+  {
+    title: 'product 3',
+    featured_image: 'product-3.jpg',
+    product_id: 052346122
+  }
+]
